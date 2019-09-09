9b41057c9888478fb802fcd5e326984860e48987
default <akoranne@gmail.com>
2019-09-09 17:32:24 -0500

    customized for my needs

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 JMETER_VERSION="5.1.1"
 
 # Example build line
-# --build-arg IMAGE_TIMEZONE="Europe/Amsterdam"
 docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} -t "askxtreme/jmeter:${JMETER_VERSION}" .
